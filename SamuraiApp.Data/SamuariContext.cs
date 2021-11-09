3a3d65c654d165cacf228e94c5815577fda4f218
John Kehinde <kehindejohn018@gmail.com>
2021-11-09 20:52:17 +0100

    Added domains and Context class

---
+++ SamuraiApp.Data/SamuariContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuariContext : DbContext
+    {
+
+    }
+}
