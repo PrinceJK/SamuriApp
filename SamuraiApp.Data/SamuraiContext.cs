4242cb32814494829d4c6263b81d1ddfa96cb769
John Kehinde <kehindejohn018@gmail.com>
2021-11-09 21:51:19 +0100

    Created database

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -8,9 +8,15 @@ using System.Threading.Tasks;
 
 namespace SamuraiApp.Data
 {
-    public class SamuariContext : DbContext
+    public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Samurai;Integrated Security=True");
+           
+        }
     }
 }
--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     internal class Program
     {
+        private static SamuraiContext _context = new SamuraiContext();
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Add: ");
+            AddSamurai();
+            GetSamurais("After Add: ");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samuari = new Samurai
+            {
+                Name = "John"
+            };
+            _context.Samurais.Add(samuari);
+            _context.SaveChanges();            
+        }
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
