0816bca5f8e1a65559a59d4f80cee28171472453
John Kehinde <kehindejohn018@gmail.com>
2021-11-09 22:41:31 +0100

    Fixed context bug

---
+++ SamuraiApp.Data/Migrations/20211109213923_init1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quotes_Samurais_SamuraiId",
+                table: "Quotes");
+
+            migrationBuilder.DropColumn(
+                name: "SamuaraiId",
+                table: "Quotes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SamuraiId",
+                table: "Quotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quotes_Samurais_SamuraiId",
+                table: "Quotes",
+                column: "SamuraiId",
+                principalTable: "Samurais",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quotes_Samurais_SamuraiId",
+                table: "Quotes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SamuraiId",
+                table: "Quotes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SamuaraiId",
+                table: "Quotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quotes_Samurais_SamuraiId",
+                table: "Quotes",
+                column: "SamuraiId",
+                principalTable: "Samurais",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,22 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SamuraiApp.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext : DbContext
+    public partial class SamuraiContext : DbContext
     {
-        public DbSet<Samurai> Samurais { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
+        public SamuraiContext()
+        {
+        }
+
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Quote> Quotes { get; set; }
+        public virtual DbSet<Samurai> Samurais { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Samurai;Integrated Security=True");
-           
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Samurai;Integrated Security=True");
+            }
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Quote>(entity =>
+            {
+                entity.HasIndex(e => e.SamuraiId, "IX_Quotes_SamuraiId");
+
+                entity.HasOne(d => d.Samurai)
+                    .WithMany(p => p.Quotes)
+                    .HasForeignKey(d => d.SamuraiId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
